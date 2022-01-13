0b5aad13fa5584080d398b25127afeaf4f907dfb
danZhur <gre4kaDISCOUNT@gmail.com>
2022-01-13 11:11:35 +0900

    adding rounds to the game

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
   var targetValue: Int = 0
   var currentValue: Int = 0
   
+  let sliderData: SliderData = SliderData(min: 1, max: 100)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    currentValue = lroundf(slider.value)
-    targetValue = Int.random(in: 1...100)
+    startNewRound()
   }
   
   @IBAction func showAlert() {
@@ -28,7 +29,9 @@ class ViewController: UIViewController {
       preferredStyle: .alert)
     let action = UIAlertAction(
       title: "Awesome",
-      style: .default)
+      style: .default) { uiAction in
+        self.startNewRound()
+      }
     
     alert.addAction(action)
     present(alert, animated: true)
@@ -36,6 +39,30 @@ class ViewController: UIViewController {
   
   @IBAction func sliderMoved(_ slider: UISlider){
     currentValue = lroundf(slider.value)
+  }
+  
+  private func startNewRound(){
+    targetValue = Int.random(in: sliderData.min...sliderData.max)
+    currentValue = sliderData.middle
+    slider.value = Float(currentValue)
+  }
+}
+
+struct SliderData{
+  let min: Int
+  let max: Int
+  let middle: Int
+  
+  init(min: Int, max: Int){
+    let middle = lroundf(Float((min + max) / 2))
+    
+    self.init(min: min, max: max, middle: middle)
+  }
+  
+  init(min: Int, max: Int, middle: Int){
+    self.min = min
+    self.max = max
+    self.middle = middle
   }
 }
 

810efdb514584bf999a63228382f3e1226d3bbcb
danZhur <gre4kaDISCOUNT@gmail.com>
2022-01-13 10:41:26 +0900

    generate the random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet var slider: UISlider!
+  var targetValue: Int = 0
   var currentValue: Int = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     currentValue = lroundf(slider.value)
+    targetValue = Int.random(in: 1...100)
   }
   
   @IBAction func showAlert() {
+    let message =  "The value of the slider is: \(currentValue)" +
+    "\nThe target value is: \(targetValue)"
+    
     let alert = UIAlertController(
       title: "Hello, World",
-      message: getMessage(),
+      message: message,
       preferredStyle: .alert)
     let action = UIAlertAction(
       title: "Awesome",
@@ -32,10 +37,5 @@ class ViewController: UIViewController {
   @IBAction func sliderMoved(_ slider: UISlider){
     currentValue = lroundf(slider.value)
   }
-  
-  private func getMessage()-> String{
-    "The value of the slider is: \(currentValue)"
-  }
-  
 }
 

e9650e3e55e09c5d8e2f719b51cc38ab6bf40189
danZhur <gre4kaDISCOUNT@gmail.com>
2022-01-13 09:55:50 +0900

    added slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  var currentValue: Int = 50
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func showAlert() {
+    let alert = UIAlertController(
+      title: "Hello, World",
+      message: getMessage(),
+      preferredStyle: .alert)
+    let action = UIAlertAction(
+      title: "Awesome",
+      style: .default)
+    
+    alert.addAction(action)
+    present(alert, animated: true)
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider){
+    currentValue = lroundf(slider.value)
+  }
+  
+  private func getMessage()-> String{
+    "The value of the slider is: \(currentValue)"
+  }
+  
 }
 
